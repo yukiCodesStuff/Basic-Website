f0a0f4b90c75f915fe679927d81965fa40f9e279
yukiCodesStuff <yuki.janvier@gmail.com>
2023-08-26 13:49:39 -0500

    added a footer and some formatting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const navbar_image = document.getElementById("navbar-image");
 let rotationDegree = 0;
 
 function spin_image() {
+    console.log("spinning")
     rotationDegree += 3600; // Increment the rotation value by 360 degrees
     navbar_image.style.transform = `rotate(${rotationDegree}deg)`; // Apply the new rotation value
 }
