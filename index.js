705871270b8186a774123d18b4b48b967b2c8571
yukiCodesStuff <yuki.janvier@gmail.com>
2023-08-26 12:19:47 -0500

    it's alive

---
+++ index.js
@@ -0,0 +1,8 @@
+const navbar_image = document.getElementById("navbar-image");
+
+let rotationDegree = 0;
+
+function spin_image() {
+    rotationDegree += 3600; // Increment the rotation value by 360 degrees
+    navbar_image.style.transform = `rotate(${rotationDegree}deg)`; // Apply the new rotation value
+}
